e13c9d6bd321063cdb5003bb54697db0e7898c36
veronika-simic <veronikasimic21@gmail.com>
2023-05-15 06:47:05 +0200

    versioning for API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-const express = require('express');
+const express = require("express");
+const v1Router = require("./v1/routes");
 const app = express();
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-    res.send('It works')
-})
+  res.send("It works");
+});
+
+app.use("/api/v1", v1Router);
 
 app.listen(PORT, () => {
-    console.log('listening on port ' + PORT)
-})
+  console.log("listening on port " + PORT);
+});
