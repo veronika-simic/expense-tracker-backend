5c64fcb92b16bab6c7f4caff58ecaa1d3f75cb7b
veronika-simic <veronikasimic21@gmail.com>
2023-05-21 06:50:43 +0200

    signup user function

--- src/models/User.js
+++ src/models/User.js
@@ -1,17 +1,30 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
-
+const bcrypt = require("bcrypt");
 const userSchema = new Schema({
   email: {
     type: String,
     required: true,
-    unique:true,
+    unique: true,
   },
   password: {
     type: String,
     required: true,
   },
 });
+
+userSchema.statics.signup = async (email, password) => {
+  const exists = await this.findOne({ email });
+  if (exists) {
+    throw Error("Email already in use");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+  return user;
+};
 
 const User = model("User", userSchema);
 module.exports = User;
